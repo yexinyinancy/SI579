ac02961d540342dbcbb8f4ba68523e4296bf868e
yexinyinancy <zj20140116@163.com>
2022-02-08 21:30:00 -0500

    A4

---
+++ A4/todo_list.js
@@ -0,0 +1,50 @@
+function addTask(description, dueTime) {
+    console.log("test")
+    const orderListElement = document.getElementById("task_list");
+    const listItem = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+   
+    span.setAttribute("class", "due");
+    button.setAttribute("class", "btn btn-sm btn-outline-danger done");
+    button.setAttribute("type", "button");
+    button.textContent = "Done";
+    button.addEventListener("click", ()=>{
+        listItem.remove();
+    });
+    listItem.textContent = description;
+    const date = new Date(dueTime);
+    span.textContent = "due " + date.toLocaleDateString() + ' ' + date.toLocaleTimeString();
+    listItem.appendChild(span);
+    listItem.appendChild(button);
+    orderListElement.appendChild(listItem);
+    descriptionInput.value = "";
+}
+
+function dateAndTimeToTimestamp(dateInputElement, timeInputElement) {
+    const dueDate = dateInputElement.valueAsNumber; // Returns the timestamp at midnight for the given date
+    const dueTime = timeInputElement.valueAsNumber; // Returns the number of milliseconds from midnight to the time
+
+    if(dueDate && dueTime) { // The user specified both a due date & due time
+        //Add the timezone offset to account for the fact that timestamps are specified by UTC
+        const timezoneOffset =  (new Date()).getTimezoneOffset() * 60 * 1000;
+        return dueDate + dueTime + timezoneOffset;
+    } else {
+        // if the user did not specify both a due date and due time, return false
+        return false;
+    }
+}
+
+const addTaskButton = document.getElementById("add_task");
+const descriptionInput = document.getElementById("task_description_input");
+const dueDateInput = document.getElementById("duedate_input");
+const dueTimeInput = document.getElementById("duetime_input");
+addTaskButton.addEventListener("click",function (){
+    addTask(descriptionInput.value, dateAndTimeToTimestamp(dueDateInput, dueTimeInput));
+});
+
+descriptionInput.addEventListener("keydown", (event)=>{
+    if (event.key === "Enter") {
+        addTask(descriptionInput.value, dateAndTimeToTimestamp(dueDateInput, dueTimeInput));
+    }
+});
